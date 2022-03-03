da665620d153ca17199b96237f4811dea8c8e839
applejinn <7applejink@gmail.com>
2022-03-03 20:19:19 +0900

    helog

--- helog/src/components/shared/Auth.js
+++ helog/src/components/shared/Auth.js
@@ -5,7 +5,23 @@ function Auth(){
     <div className={styles.container}>
       <div className={styles.auth}>
         <div className={styles.wrapper}>
-          
+          <div className={styles.gray-block}>
+
+          </div>
+          <div className={styles.white-block}>
+            <div className={styles.exit-wrapper}>
+              
+            </div>
+            <form>
+              <input type="text" placeholder="Email" required />
+              <input type="password" placeholder="Password" required />
+              <input type="submit" value="Log In" />
+            </form>
+          </div>
+          <div>
+            <button>Continue with Google</button>
+            <button>Continue with Github</button>
+          </div>
         </div>
       </div>
     </div>
