b539514d419bfb3a2836e027da1b359533f56964
applejinn <7applejink@gmail.com>
2022-03-04 20:19:19 +0900

    helog

--- helog/src/components/shared/Auth.js
+++ helog/src/components/shared/Auth.js
@@ -5,22 +5,22 @@ function Auth(){
     <div className={styles.container}>
       <div className={styles.auth}>
         <div className={styles.wrapper}>
-          <div className={styles.gray-block}>
+          <div className={styles.grayBlock}>
 
           </div>
-          <div className={styles.white-block}>
-            <div className={styles.exit-wrapper}>
-              
+          <div className={styles.whiteBlock}>
+            <div className={styles.exitWrapper}>
+
             </div>
             <form>
               <input type="text" placeholder="Email" required />
               <input type="password" placeholder="Password" required />
               <input type="submit" value="Log In" />
             </form>
-          </div>
-          <div>
-            <button>Continue with Google</button>
-            <button>Continue with Github</button>
+            <div>
+              <button>Continue with Google</button>
+              <button>Continue with Github</button>
+            </div>
           </div>
         </div>
       </div>
--- helog/src/components/shared/Layout.js
+++ helog/src/components/shared/Layout.js
@@ -8,7 +8,7 @@ function Layout({isLoggedIn, children}){
   return(
     <>
       {
-        modal
+        modal === true
         ? <Auth/>
         : null
       }
