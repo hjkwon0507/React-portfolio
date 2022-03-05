cb41fab661cc201c9f5207c7cace803d7c7b7878
applejinn <7applejink@gmail.com>
2022-03-05 20:19:19 +0900

    helog

--- helog/src/components/shared/Auth.js
+++ helog/src/components/shared/Auth.js
@@ -12,14 +12,18 @@ function Auth(){
             <div className={styles.exitWrapper}>
 
             </div>
-            <form>
-              <input type="text" placeholder="Email" required />
-              <input type="password" placeholder="Password" required />
-              <input type="submit" value="Log In" />
-            </form>
-            <div>
-              <button>Continue with Google</button>
-              <button>Continue with Github</button>
+            <div className={styles.blockContent}>
+              <div className={styles.content}>
+                <form className={styles.upperWrapper}>
+                  <input type="text" placeholder="Email" required />
+                  <input type="password" placeholder="Password" required />
+                  <input type="submit" value="Log In" />
+                </form>
+                <div>
+                  <button>Continue with Google</button>
+                  <button>Continue with Github</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
